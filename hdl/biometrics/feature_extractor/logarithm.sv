c24d9685e12e9b1d4acec30a7f5f12b562950cb5
Andi Qu <andiqu@mit.edu>
2023-11-08 23:12:41 -0500

    Implement Joe's bootleg logarithm

---
+++ hdl/biometrics/feature_extractor/logarithm.sv
@@ -0,0 +1,69 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+/*
+ * Module `logarithm
+ *
+ * Estimates the base-2 logarithm of a number via leading-bit
+ * detection and linear interpolation.
+ */
+module logarithm (
+  input wire clk_in,
+  input wire rst_in,
+
+  input wire [31:0] filtered_data_in,
+  input wire filtered_valid_in,
+  output logic filtered_ready_out,
+
+  input wire log_ready_in,
+  output logic log_valid_out,
+  output logic signed [15:0] log_data_out
+);
+
+  logic signed [5:0] res_int; // One more bit than strictly necessary
+  logic [15:0] half;
+  logic [7:0] quarter;
+  logic [3:0] eighth;
+
+  logic [10:0] res_frac;
+  logic [31:0] shifted;
+
+  always_comb begin
+    // Calculate the integer part
+    // TODO: make this more general (handle different precisions)
+    // (Right now we assume 16 int bits + 16 frac bits in input)
+    res_int = -16;
+    shifted = filtered_data_in;
+
+    if (|shifted[31:16]) res_int = res_int + 16;
+    else shifted = shifted << 16;
+
+    if (|shifted[31:24]) res_int = res_int + 8;
+    else shifted = shifted << 8;
+
+    if (|shifted[31:28]) res_int = res_int + 4;
+    else shifted = shifted << 4;
+
+    if (|shifted[31:30]) res_int = res_int + 2;
+    else shifted = shifted << 2;
+
+    if (shifted[31]) res_int = res_int + 1;
+    else shifted = shifted << 1;
+
+    // Calculate the fractional part
+    res_frac = shifted[30:20];
+  end
+
+  always_ff @(posedge clk_in) begin
+    if (rst_in) begin
+      log_data_out <= 0;
+    end else begin
+      log_data_out <= {res_int[4:0], res_frac};
+      log_valid_out <= filtered_valid_in;
+      filtered_ready_out <= log_ready_in;
+    end
+  end
+
+endmodule
+
+`default_nettype wire
---
+++ sim/logarithm_tb.sv
@@ -0,0 +1,109 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+module logarithm_tb;
+
+  // Input logics
+  logic clk_in, rst_in;
+  logic filtered_valid_in, log_ready_in;
+  logic [31:0] filtered_data_in;
+  // Output logics
+  logic filtered_ready_out, log_valid_out;
+  logic signed [15:0] log_data_out;
+
+  logarithm uut (
+    .clk_in(clk_in),
+    .rst_in(rst_in),
+
+    .filtered_data_in(filtered_data_in),
+    .filtered_valid_in(filtered_valid_in),
+    .filtered_ready_out(filtered_ready_out),
+
+    .log_ready_in(log_ready_in),
+    .log_valid_out(log_valid_out),
+    .log_data_out(log_data_out)
+  );
+
+  always begin
+      #5; // 100MHz clock
+      clk_in = !clk_in;
+  end
+
+  initial begin
+    $dumpfile("logarithm.vcd");
+    $dumpvars(0, logarithm_tb);
+    $display("Starting Sim");
+
+    // Initialize variables
+    clk_in = 0;
+    rst_in = 0;
+    filtered_valid_in = 0;
+    log_ready_in = 0;
+    filtered_data_in = 1;
+
+    // Wait a lil bit and reset
+    #10;
+    rst_in = 1;
+    #10;
+    rst_in = 0;
+    #10;
+
+    // Small values
+    filtered_valid_in = 1;
+    log_ready_in = 1;
+    for (int i = 1; i < 1024; i = i + 1) begin
+      filtered_data_in = i;
+      #10;
+    end
+    filtered_valid_in = 0;
+    filtered_data_in = 1;
+    #10;
+    log_ready_in = 0;
+    #50;
+
+    // Medium-small values
+    filtered_valid_in = 1;
+    log_ready_in = 1;
+    for (int i = 1024; i < 65_536; i = i + 64) begin
+      filtered_data_in = i;
+      #10;
+    end
+    filtered_valid_in = 0;
+    filtered_data_in = 1;
+    #10;
+    log_ready_in = 0;
+    #50;
+
+    // Medium-large values
+    filtered_valid_in = 1;
+    log_ready_in = 1;
+    for (int i = 65_536; i < 2_097_152; i = i + 2048) begin
+      filtered_data_in = i;
+      #10;
+    end
+    filtered_valid_in = 0;
+    filtered_data_in = 1;
+    #10;
+    log_ready_in = 0;
+    #50;
+
+    // Large values
+    filtered_valid_in = 1;
+    log_ready_in = 1;
+    for (int i = 2_097_152; i < 1_073_741_824; i = i + 2_097_152) begin
+      filtered_data_in = i;
+      #10;
+    end
+    filtered_valid_in = 0;
+    filtered_data_in = 1;
+    #10;
+    log_ready_in = 0;
+    #50;
+
+    $display("Finishing Sim");
+    $finish;
+  end
+
+endmodule
+
+`default_nettype wire
