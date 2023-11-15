2b6cb0c8f891cc00d4d26ad8129af86d6122f1f2
Andi Qu <andiqu@mit.edu>
2023-11-15 17:23:52 -0500

    Add Vivado build scripts

---
+++ build.py
@@ -0,0 +1,39 @@
+import os
+import subprocess
+
+vivado = "/opt/Xilinx/Vivado/2023.1/bin/vivado"
+
+
+class VivadoBuildError(Exception):
+    pass
+
+
+if not os.access("synth_feature_extractor.tcl", os.R_OK):
+    raise VivadoBuildError("you should pass a build script for us to run!")
+
+print("starting vivado, please wait...")
+proc = subprocess.Popen(f"{vivado} -mode batch -source synth_feature_extractor.tcl",
+                        shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+while True:
+    line = proc.stdout.readline()
+    if not line:
+        break
+    else:
+        print(str(line.rstrip(), encoding='ascii'))
+
+proc.wait()
+
+if proc.returncode != 0:
+    save("vivado.log")
+    raise VivadoBuildError(f"vivado exited with code {proc.returncode}")
+else:
+    save("vivado.log")
+
+
+# look for out.bit, because we've hard coded that for now i guess
+if not os.access("obj/final.bit", os.R_OK):
+    raise VivadoBuildError(
+        "vivado exited successfully, but no out.bit generated")
+
+save("obj/final.bit")
